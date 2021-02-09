039eacfd2d1ca2e5695578ed8404eb8d3652ae0a
Albert Gergo <gergoalbert.gta@gmail.com>
2021-02-09 20:22:55 +0200

    feat: colors

---
+++ client/constants/style/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  codGray: "#131313", // really dark gray
+  mineShaft: "#2B2B2B", // dark gray
+  jumbo: "#86868B", // light gray
+  frenchGray: "#c1c1c9", // really light gray
+  blueRibbon: "#1561F0", // bright blue
+  electricViolet: "#4D15F0",
+};
