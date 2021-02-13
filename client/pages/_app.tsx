62115c4b2118bfa479dd635044a5644b26337a53
Albert Gergo <gergoalbert.gta@gmail.com>
2021-02-13 15:34:17 +0200

    refactor: using next.js head component

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,16 +1,17 @@
 import { GlobalStyle } from "../styles/globals";
-import Helmet from "react-helmet";
+//import Helmet from "react-helmet";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Helmet>
+      <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
           rel="stylesheet"
         />
-      </Helmet>
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
